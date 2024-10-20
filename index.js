7f41069d294c52133e56a35d7abdd71909bd7c37
NikBel3476 <nikita.belkin3476@gmail.com>
2024-10-20 16:13:17 +0400

    feature: add image view

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// on page scroll animation
 document.querySelectorAll('.scroll-animation-wrapper').forEach(element => {
     element.classList.remove('scroll-animation-wrapper-transition');
 
@@ -12,4 +13,23 @@ document.querySelectorAll('.scroll-animation-wrapper').forEach(element => {
         });
     });
     observer.observe(element);
+});
+
+// fullscreen image view
+const image_view = document.getElementById('image-view');
+image_view.addEventListener('click', e => {
+    e.target.style.zIndex = -5;
+});
+image_view.addEventListener('wheel', e => {
+    e.preventDefault();
+});
+image_view.addEventListener('scroll', e => {
+    e.preventDefault();
+});
+
+document.querySelectorAll('.type-card__img').forEach(img => {
+    img.addEventListener('click', e => {
+        image_view.style.backgroundImage = `url(${e.target.src})`;
+        image_view.style.zIndex = 5;
+    });
 });
